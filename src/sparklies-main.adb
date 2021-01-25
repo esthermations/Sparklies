30fd5c6e8d25b26384bfa91bed6ddd08ba953f77
Esther O'Keefe <esther@okeefe.earth>
2021-01-25 22:45:33 +1100

    More accurate names

--- src/sparklies-main.adb
+++ src/sparklies-main.adb
@@ -5,23 +5,27 @@ with Sparklies.Units; use Sparklies.Units;
 
 procedure Sparklies.Main is
 
+   ns : constant Units.Time := (s * 1.0E-9);
+
+   --  NOTE: Nanoseconds is chosen here because on my system
+   --  Ada.Real_Time.Time_Unit is equal to 1.0E-9, which is one nanosecond.
    function To_Time_Unit (TS : in Time_Span) return Units.Time is
-      (Sparklies.Units.Mks_Type (TS / Nanoseconds (1)) * s * 1.0e-9);
+      (Sparklies.Units.Mks_Type (TS / Nanoseconds (1)) * ns);
 
    L     :          Units.Length := 0.0 * m;
    Speed : constant Units.Speed  := 2.0 * m / s;
+   DT    :          Units.Time;
 
-   Initial : constant Ada.Real_Time.Time := Clock;
-   Now     :          Ada.Real_Time.Time := Initial;
-   DT      :          Units.Time;
-   Final   : constant Ada.Real_Time.Time := Initial + Seconds (5);
+   Initial  : constant Ada.Real_Time.Time := Clock;
+   Previous :          Ada.Real_Time.Time := Initial;
+   Final    : constant Ada.Real_Time.Time := Initial + Seconds (5);
 begin
    loop
-      exit when Now > Final;
+      exit when Previous > Final;
 
-      DT  := To_Time_Unit (Clock - Now);
-      Now := Clock;
-      L   := L + (Speed * DT);
+      DT       := To_Time_Unit (Clock - Previous);
+      Previous := Clock;
+      L        := L + (Speed * DT);
 
       Put ("L = " & L'Img & " metres");
       New_Line;
